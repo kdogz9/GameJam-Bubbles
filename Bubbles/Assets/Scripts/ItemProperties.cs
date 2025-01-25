bb27aa663bc2ff60b76258c531a01ec26fd5ca45
kdogz9 <2401317@students.ucreative.ac.uk>
2025-01-25 15:44:46 +0000

    coral

--- Bubbles/Assets/Scripts/ItemProperties.cs
+++ Bubbles/Assets/Scripts/ItemProperties.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class ItemProperties : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool hasBeenPlaced = false; // Tracks if the item has already been placed
 }
+
